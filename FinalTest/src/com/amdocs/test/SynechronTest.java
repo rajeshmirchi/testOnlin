dff65eb6bbf9c3b0ee7f780e2ab01e6618b96066
RAJESHKU <RAJESHKU@RAJESHKU07.corp.amdocs.com>
2021-02-26 18:04:52 +0530

    First Pushing

---
+++ FinalTest/src/com/amdocs/test/SynechronTest.java
@@ -0,0 +1,30 @@
+package com.amdocs.test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SynechronTest {
+	
+	public static void main(String... args) {
+
+		//System.out.println("Please enter number");
+//		Scanner sc = new Scanner(System.in);
+		int number = 100; //sc.nextInt();
+		
+		List<Integer> primes = IntStream.rangeClosed(2, number)
+//				.filter(n -> n%2 !=0)
+                .filter(n -> isPrime(n))
+                .boxed()
+                .collect(Collectors.toList());
+		
+		System.out.println(primes);
+	}
+
+	public static boolean isPrime(int number) {
+		//System.out.println("Number used:-"+number);
+	    return IntStream.rangeClosed(2, (int)Math.sqrt(number)).noneMatch(i -> number%i == 0); 
+	}
+}
+
+// 100 -25 , 1000 - 168, 10000 - 1229
